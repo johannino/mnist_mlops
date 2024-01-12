81721920636bb4c3d72aa15dc32239e87eeba969
johannino <johanninov@gmail.com>
2024-01-12 20:49:38 +0100

    Exercises in CI v2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,4 @@
-# tests/__init__.py
 import os
-
-_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-_TEST_ROOT = os.path.join(_TEST_ROOT, "mnist_mlops")  # root of test folder
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
-_PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")  # root of data
+_TEST_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")
